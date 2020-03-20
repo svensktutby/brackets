11d14245700d3f2bf63f859361764dc67cbd8441
svensktutby <svensk.tut.by@gmail.com>
2020-03-20 11:06:00 +0300

    feat: solve Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const unitBracketsConfig = bracketsConfig.map(item => item.join(''));
+  const halfStrLength = str.length / 2;
+
+  for (let i = 0; i < halfStrLength; i++) {
+    for (const pair of unitBracketsConfig) {
+      str = str.replace(pair, '');
+    }
+  }
+
+  return !str.length;
 }
